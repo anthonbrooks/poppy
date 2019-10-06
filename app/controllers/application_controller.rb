c9467f14db2db19b3ab2a5d8561424d758ac1742
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-05 23:05:21 -0700

    added a basic form for new reviews

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,18 @@
+# frozen_string_literal: true
+
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
   end
 
+  get '/new_review' do
+    erb :new_review
+  end
 end

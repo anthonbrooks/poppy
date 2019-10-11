87c0fdc7bf3a87d959ca7b91c57052bd4ebabe4a
Anthony Prescott-Brooks <anthonybrooks226@gmail.com>
2019-10-11 10:47:44 -0700

    A user can delete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -13,15 +13,13 @@ class ReviewsController < ApplicationController
     erb :reviews
   end
 
-  post '/reviews/:id' do
+  post '/reviews' do
     @title = params[:title]
     @director = params[:director]
     @fav_character = params[:fav_character]
     @description = params[:description]
     @rating = params[:rating]
-    @review = current_user.reviews.build(title: @title, director: @director,\
-                                         fav_character: @fav_character, description: @description,\
-                                         rating: @rating)
+    @review = current_user.reviews.create(title: @title, director: @director, fav_character: @fav_character, description: @description, rating: @rating)
     if @review.save
       redirect "/reviews/#{ @review.id }"
     else
@@ -33,20 +31,12 @@ class ReviewsController < ApplicationController
     @review = Review.find_by(id: params[:id])
     erb :'/reviews/show'
   end
-  
-  patch '/articles/:id' do
-    @review = Review.find_by(id: params[:id])
-    @review.update(title: @title, director: @director,\
-                    fav_character: @fav_character, description: @description,\
-                    rating: @rating)
-    redirect "/reviews/#{ @review.id }"
-  end
 
   get '/reviews/:id/edit' do
     if !logged_in
       redirect '/'
     else
-      @review = current_user.reviews.find_by(params[:id])
+      @review = current_user.reviews.find_by_id(params[:id])
       if @review
         erb :'/reviews/edit'
       else
@@ -54,10 +44,36 @@ class ReviewsController < ApplicationController
       end
     end
   end
-    
+
+  patch '/reviews/:id' do
+    @review = Review.find_by_id(params[:id])
+    @review.update(title: params[:title])
+    @review.director = params[:director]
+    @review.fav_character = params[:fav_character]
+    @review.description = params[:description]
+    @review.update(rating: params[:rating])
+    @review.save
+    redirect "/reviews/#{ @review.id }"
+  end
+
+  post '/reviews/:id' do
+    @review = Review.find_by_id(params[:id])
+    @review.title = params[:title]
+    @review.director = params[:director]
+    @review.fav_character = params[:fav_character]
+    @review.description = params[:description]
+    @review.rating = params[:rating]
+    @review.save
+    redirect "/reviews/#{ @review.id }"
+  end
+  
+  get '/reviews/:id/delete' do
+    @review = Review.find_by_id(params[:id])
+    erb :'/reviews/delete'
+  end
+  
   delete '/reviews/:id' do
-    @review = Review.find_by(id: params[:id])
-    @review.destroy(params[:id])
-    redirect '/users/home'
+    @review = Review.find_by_id(params[:id]).destroy
+    redirect '/reviews'
   end
 end
